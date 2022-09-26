d0f6114b73ac53c71cc1b0eb0895ffa46f8fc2ee
Soeng-Gam Lu <soenggam@outlook.com>
2022-09-26 17:03:35 +0800

    add libinit for china and india variants

---
+++ libinit/init_sm6375.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vector>
+#include <string>
+#include <fstream>
+
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include "property_service.h"
+#include "vendor_init.h"
+
+using android::init::property_set;
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void property_override_dual(char const system_prop[], char const vendor_prop[],
+    char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
+void load_RMX3471() {
+    property_override("ro.build.product", "RMX3471");
+    property_override("ro.product.device", "RMX3471");
+}
+
+void load_RMX3478() {
+    property_override("ro.build.product", "RMX3478");
+    property_override("ro.product.device", "RMX3478");
+}
+
+
+void vendor_load_properties() {
+    std::ifstream infile("/proc/oplusVersion/prjVersion");
+    std::string prjName;
+    getline(infile, prjName);
+
+    if (prjName == "21707") {
+        load_RMX3471();
+    } else if (prjName == "2162B") {
+        load_RMX3478();
+    } else {
+        LOG(ERROR) << __func__ << ": unexcepted prjVersion!";
+    }
+}
